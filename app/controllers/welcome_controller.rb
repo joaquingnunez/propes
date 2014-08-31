2bc3fe7ed89b084429df05af6f871051857b4d7e
Joaquin Gonzalez <joaquin.gonzalez.n@gmail.com>
2014-08-30 20:54:43 -0400

    version 3

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
 	def index
 		@publicacion =  Publicacion.all.order(created_at: :desc)
+		@ultimas = Publicacion.find(:all, :order => "id desc", :limit => 5)
 		if user_signed_in?
 			@user = current_user
 		end
